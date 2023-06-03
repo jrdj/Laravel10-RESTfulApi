9e45b8e97140c163afc3dad0ffa483c538b240eb
Mc Jared Jamila <28791171+jrdj@users.noreply.github.com>
2023-06-04 04:58:29 +0800

    Update SaleController
    move SaleController to api/v1 and corrects namespaces

--- routes/api.php
+++ routes/api.php
@@ -18,8 +18,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-
 Route::group(['middleware' => 'auth:sanctum'], function(){
         Route::prefix('v1')->group(function(){
         Route::apiResource('/tasks', TaskController::class);
